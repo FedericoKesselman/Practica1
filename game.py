869ed889ea8660ce264972465ce527d0bb6673d4
Federico Kesselman <kesselmanfederico@gmail.com>
2024-03-20 14:49:13 -0300

    Últimas pequeñas correciones al codigo

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ words = ["python", "programación", "computadora", "código", "desarrollo", "int
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Contador de intentos del usuario 
-attempts = 0
+failed_attempts = 0 
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 
@@ -25,33 +25,36 @@ elif difficulty == 2:
     guessed_letters.append(secret_word[-1])
 else:
     # Se muestran vocales. Son agregadas a la lista de letras
-    vowels = "aeiou"
-    word_displayed = "".join([letter if letter in vowels else "_" for letter in secret_word]) 
-    
+    word_displayed = "".join([letter if letter in 'aeiou' else "_" for letter in secret_word]) 
+
     for letter in secret_word:
-        if letter in vowels:
+        if letter in 'aeiou':
                 guessed_letters.append(letter)
 
 print(f"Palabra: {word_displayed}")
 
-while attempts < 10:
+while failed_attempts < 10:
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
 
      # Verificar si la letra ya ha sido adivinada
-    if letter in guessed_letters and letter != "":
+    if letter in guessed_letters:
         print("Ya cuentas con esa letra. Intenta con otra.")
+        continue
+    
+    if letter == "":
+        print("Error. No se ingreso ninguna letra.")
         continue
 
     # Agregar la letra a la lista de letras adivinadas
     guessed_letters.append(letter)
 
     # Verificar si la letra está en la palabra secreta
-    if letter in secret_word and letter != "":
+    if letter in secret_word:
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
-        attempts = attempts + 1 
+        failed_attempts += 1 
 
     # Mostrar la palabra parcialmente adivinada
     letters = []
